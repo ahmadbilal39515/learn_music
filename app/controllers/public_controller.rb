3e20a731529a45ba22e5cdfd835cdd3e160cece5
Robert Cekay <robertcekay@gmail.com>
2023-01-25 19:36:09 -0600

    * add platform x86_64-linux * Add Navbar and basic layouts

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,4 +3,8 @@ class PublicController < ApplicationController
   def index
 
   end
+
+  def realmajik
+    render layout: "one_page"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resource :public
+
+  get '/realmajik', to: 'public#realmajik'
+
   # Defines the root path route ("/")
   root "public#index"
 end
